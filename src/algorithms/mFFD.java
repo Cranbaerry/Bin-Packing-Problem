e3c9e4bf497be65dc7df394d2ae8eda8ab06670a
Colossies <127578135+Colossies@users.noreply.github.com>
2024-04-25 18:36:16 +0800

    mFFD

--- src/algorithms/mFFD.java
+++ src/algorithms/mFFD.java
@@ -27,10 +27,7 @@ public class mFFD implements Algorithm {
 
     public mFFD() {
         // Initialize custom parameters here
-        largeItems = new ArrayList<Integer>();
-        mediumItems = new ArrayList<Integer>();
-        smallItems = new ArrayList<Integer>();
-        tinyItems = new ArrayList<Integer>();
+        
     }
 
     @Override
@@ -38,21 +35,30 @@ public class mFFD implements Algorithm {
         Solution solution = new Solution(problem); // This should be at the top, it initializes the runtime timer
         this.binCapacity = problem.getCapacity();
         HashMap<Integer, Integer> problemItems = problem.items.getItems();
-        for (int weight : problemItems.keySet()) { // convert hashmap into array of integers
-        	for(int i = 0; i < problemItems.get(weight);i++) {
+        int count = 0;
+        largeItems = new ArrayList<Integer>();
+        mediumItems = new ArrayList<Integer>();
+        smallItems = new ArrayList<Integer>();
+        tinyItems = new ArrayList<Integer>();
+        for(Map.Entry<Integer, Integer> entry: problemItems.entrySet()) {
+        	int weight = entry.getKey();
+        	for(int i = 0; i < entry.getValue(); i++) {
         		if(weight > this.binCapacity / 2) {
         			largeItems.add(weight);
+        			count++;
         		}
         		else if(weight > this.binCapacity / 3) {
         			mediumItems.add(weight);
+        			count++;
         		}
         		else if(weight > this.binCapacity / 4) {
         			smallItems.add(weight);
+        			count++;
         		}
-        		else {
+        		else if(weight <= this.binCapacity / 4){
         			tinyItems.add(weight);
+        			count++;
         		}
-        		
         	}
         }
         Collections.sort(largeItems, Collections.reverseOrder());
@@ -60,75 +66,101 @@ public class mFFD implements Algorithm {
         Collections.sort(smallItems, Collections.reverseOrder());
         Collections.sort(tinyItems, Collections.reverseOrder());
         
-        int currentBinCapacity = 0;
-        HashMap<ItemFactory, Integer> bins = new HashMap<>();
-
+        
+        ArrayList<Integer> weights = new ArrayList<Integer>();
+        ArrayList<ItemFactory> bins = new ArrayList<ItemFactory>();
+//        HashMap<ItemFactory, Integer> bins = new HashMap<>();
+        int itemIterated = 0;
         for(int weight: largeItems){
             ItemFactory bin = new ItemFactory();
             bin.addItem(weight, 1);
-            bins.put(bin, weight);
+            bins.add(bin);
+            weights.add(weight);
+            itemIterated++;
         }
         
         for(int weight: mediumItems) {
         	boolean placed = false;
-        	for(Map.Entry<ItemFactory, Integer> entry: bins.entrySet()) {
-        		Integer capacity = entry.getValue();
+        	for(int i = 0; i < bins.size(); i++) {
+        		Integer capacity = weights.get(i);
         		if(capacity + weight <= this.binCapacity) {
         			placed = true;
         			capacity += weight;
-        			entry.getKey().addItem(weight, 1);
-        			entry.setValue(capacity);
+        			ItemFactory bin = bins.get(i);
+        			bin.addItem(weight, 1);
+        			bins.set(i, bin);
+        			weights.set(i, capacity);
+        			itemIterated++;
+        			break;
         		}
         	}
         	
-        	if(placed == true) continue;
-        	ItemFactory bin = new ItemFactory();
-            bin.addItem(weight, 1);
-            bins.put(bin, weight);
-           
+        	if(!placed) {
+        		ItemFactory bin = new ItemFactory();
+                bin.addItem(weight, 1);
+                bins.add(bin);
+                weights.add(weight);
+                itemIterated++;
+        	}
         	
         }
         
         for(int weight: smallItems) {
         	boolean placed = false;
-        	for(Map.Entry<ItemFactory, Integer> entry: bins.entrySet()) {
-        		Integer capacity = entry.getValue();
+        	for(int i = 0; i < bins.size(); i++) {
+        		Integer capacity = weights.get(i);
         		if(capacity + weight <= this.binCapacity) {
         			placed = true;
         			capacity += weight;
-        			entry.getKey().addItem(weight, 1);
-        			entry.setValue(capacity);
+        			ItemFactory bin = bins.get(i);
+        			bin.addItem(weight, 1);
+        			bins.set(i, bin);
+        			weights.set(i, capacity);
+        			itemIterated++;
+        			break;
         		}
         	}
         	
-        	if(placed == true) continue;
-        	ItemFactory bin = new ItemFactory();
-            bin.addItem(weight, 1);
-            bins.put(bin, weight);
-        	
+        	if(!placed) {
+        		ItemFactory bin = new ItemFactory();
+                bin.addItem(weight, 1);
+                bins.add(bin);
+                weights.add(weight);
+                itemIterated++;
+        	}
         }
         
         for(int weight: tinyItems) {
         	boolean placed = false;
-        	for(Map.Entry<ItemFactory, Integer> entry: bins.entrySet()) {
-        		Integer capacity = entry.getValue();
+        	for(int i = 0; i < bins.size(); i++) {
+        		Integer capacity = weights.get(i);
         		if(capacity + weight <= this.binCapacity) {
         			placed = true;
         			capacity += weight;
-        			entry.getKey().addItem(weight, 1);
-        			entry.setValue(capacity);
+        			ItemFactory bin = bins.get(i);
+        			bin.addItem(weight, 1);
+        			bins.set(i, bin);
+        			weights.set(i, capacity);
+        			itemIterated++;
+        			break;
         		}
         	}
         	
-        	if(placed == true) continue;
-        	ItemFactory bin = new ItemFactory();
-            bin.addItem(weight, 1);
-            bins.put(bin, weight);
+        	if(!placed) {
+        		ItemFactory bin = new ItemFactory();
+                bin.addItem(weight, 1);
+                bins.add(bin);
+                weights.add(weight);
+                itemIterated++;
+        	}
         	
         }
         
-        for(ItemFactory bin: bins.keySet()) {
-        	System.out.println(bin.getNumberOfItems());
+        System.out.println(itemIterated);
+        
+        for(int i = 0; i < bins.size(); i++) {
+        	ItemFactory bin = bins.get(i);
+//        	System.out.println(bin.getNumberOfItems());
         	solution.bins.createBin(bin);
         }
         
